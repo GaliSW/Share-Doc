1b173f79cd21448b67873c98b53d2c1544c89f65
ProfetAI-NB-037\gary <gary@profetai.com>
2024-07-03 18:16:56 +0800

    update: footer copy write

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -63,7 +63,7 @@ const config: Config = {
     },
     footer: {
       style: "dark",
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Share Doc, Inc. Built with Gali.`,
     },
     prism: {
       theme: prismThemes.github,
