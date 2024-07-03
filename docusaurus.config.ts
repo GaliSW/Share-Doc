a4c217ed5d16fc5c87e1892c30e2578c59672e1d
ProfetAI-NB-037\gary <gary@profetai.com>
2024-07-03 18:23:16 +0800

    fixed: lower case string

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "Share DOc",
+  title: "Share Doc",
   tagline: "Share Doc",
   favicon: "img/favicon.ico",
 
