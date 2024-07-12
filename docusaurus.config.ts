04f250c42be7c33acbd6f1fa43c809ac523cdbf4
GaliSW <gary850204@gmail.com>
2024-07-12 23:35:40 +0800

    remove: i18n setting

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,109 +3,89 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "Share Doc",
-  tagline: "Share Doc",
-  favicon: "img/favicon.ico",
+    title: "Share Doc",
+    tagline: "Share Doc",
+    favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
-  url: "https://sharedoc.zeabur.app/",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+    // Set the production url of your site here
+    url: "https://sharedoc.zeabur.app/",
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "facebook", // Usually your GitHub org/user name.
+    projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "zh-Hant",
-    locales: ["en", "zh-Hant"],
-    path: "i18n",
-    localeConfigs: {
-      en: {
-        label: "English",
-        direction: "ltr",
-        htmlLang: "en-US",
-        calendar: "gregory",
-        path: "en",
-      },
-      "zh-Hant": {
-        label: "繁體中文",
-        direction: "ltr",
-        htmlLang: "zh-Hant",
-        calendar: "chinese",
-        path: "zh-Hant",
-      },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "zh-Hant",
+        locales: ["zh-Hant"],
+        path: "i18n",
     },
-  },
 
-  presets: [
-    [
-      "classic",
-      {
-        docs: {
-          sidebarPath: "./sidebars.ts",
-          routeBasePath: "/",
-        },
-        theme: {
-          customCss: "./src/css/custom.css",
-        },
-      } satisfies Preset.Options,
+    presets: [
+        [
+            "classic",
+            {
+                docs: {
+                    sidebarPath: "./sidebars.ts",
+                    routeBasePath: "/",
+                },
+                theme: {
+                    customCss: "./src/css/custom.css",
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  themes: [
-    [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
-      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
-      {
-        hashed: true,
-        language: ["en", "zh"],
-        indexDocs: true,
-        highlightSearchTermsOnTargetPage: true,
-        explicitSearchResultPath: true,
-        docsRouteBasePath: "/",
-      },
+    themes: [
+        [
+            require.resolve("@easyops-cn/docusaurus-search-local"),
+            /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+            {
+                hashed: true,
+                language: ["en", "zh"],
+                indexDocs: true,
+                highlightSearchTermsOnTargetPage: true,
+                explicitSearchResultPath: true,
+                docsRouteBasePath: "/",
+            },
+        ],
     ],
-  ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
-    navbar: {
-      title: "Share Doc",
-      logo: {
-        alt: "My Site Logo",
-        src: "img/logo.png",
-      },
-      items: [
-        {
-          type: "localeDropdown",
-          position: "right",
+    themeConfig: {
+        // Replace with your project's social card
+        image: "img/docusaurus-social-card.jpg",
+        navbar: {
+            title: "Share Doc",
+            logo: {
+                alt: "My Site Logo",
+                src: "img/logo.png",
+            },
+            items: [
+                {
+                    href: "https://github.com/GaliSW/Share-Doc",
+                    label: "GitHub",
+                    position: "right",
+                },
+            ],
         },
-        {
-          href: "https://github.com/GaliSW/Share-Doc",
-          label: "GitHub",
-          position: "right",
+        footer: {
+            style: "dark",
+            copyright: `Copyright © ${new Date().getFullYear()} Share Doc, Inc. Built with Gali.`,
         },
-      ],
-    },
-    footer: {
-      style: "dark",
-      copyright: `Copyright © ${new Date().getFullYear()} Share Doc, Inc. Built with Gali.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
